dd8dfea587e94d9476d688b0cef63dd269739ec0
Minjun Youn <minjun1.y@partner.samsung.com>
2019-04-02 17:31:37 -0500

    Add route guard

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Route, Switch } from "react-router-dom";
 import NotFound from './containers/NotFound';
-import AppliedRoute from './components/AppliedRoute'
+import AuthenticatedRoute from './components/AuthenticatedRoute'
+import UnauthenticatedRoute from './components/UnauthenticatedRoute'
 import Home from './containers/Home';
 import Login from './containers/Login'
 import New from './containers/Resident/New'
@@ -11,11 +12,11 @@ import Announcement from './containers/Apart/Announcement';
 const Routes = ({ childProps }) => {
   return (
     <Switch>
-      <AppliedRoute exact path='/' component={Home} props={childProps} />
-      <AppliedRoute path='/login' component={Login} props={childProps} />
-      <AppliedRoute path='/new-resident' component={New} props={childProps} />
-      <AppliedRoute path='/aparts' component={ApartInfo} props={childProps} />
-      <AppliedRoute path='/announcement' component={Announcement} props={childProps} />
+      <AuthenticatedRoute exact path='/' component={Home} props={childProps} />
+      <UnauthenticatedRoute path='/login' component={Login} props={childProps} />
+      <AuthenticatedRoute path='/new-resident' component={New} props={childProps} />
+      <AuthenticatedRoute path='/aparts' component={ApartInfo} props={childProps} />
+      <AuthenticatedRoute path='/announcement' component={Announcement} props={childProps} />
       <Route component={NotFound} />
     </Switch>
   )
